3ae52bc45fde11146e3b4622f79fc0c1adc2ec36
perpouh <perpouh@gmail.com>
2021-03-22 12:15:05 +0900

    スタイルが当たる状態に修正。環境差？わからない

--- data/www/src/main.js
+++ data/www/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import http from './http'
 
-// import 'musubii/src/scss/musubii.scss'
+import 'musubii/src/scss/musubii.scss'
 
 const app = createApp(App)
 app.use(router)
