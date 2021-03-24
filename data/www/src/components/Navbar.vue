4451d58c78e0902a76528368d0831310c9d0a6a8
perpouh <perpouh@gmail.com>
2021-03-24 15:51:04 +0900

    fontawesome導入

--- data/www/src/components/Navbar.vue
+++ data/www/src/components/Navbar.vue
@@ -1,15 +1,39 @@
 <template>
   <ul class="navbar">
-    <li class="navbar__item"><i></i><span>ホーム</span></li>
-    <li class="navbar__item"><i></i><span>検索</span></li>
-    <li class="navbar__item"><i></i><span>投稿</span></li>
-    <li class="navbar__item"><i></i><span>通知</span></li>
-    <li class="navbar__item"><i></i><span>設定</span></li>
+    <li class="navbar__item">
+      <a href="#">
+        <span class="navbar__item__icon"><font-awesome-icon icon="home"/></span><br class="sp-only"><span class="navbar__item__text">ホーム</span>
+      </a>
+    </li>
+    <li class="navbar__item">
+      <a href="#">
+        <span class="navbar__item__icon"><font-awesome-icon icon="search"/></span><br class="sp-only"><span class="navbar__item__text">検索</span>
+      </a>
+    </li>
+    <li class="navbar__item">
+      <a href="#">
+        <span class="navbar__item__icon"><font-awesome-icon icon="edit"/></span><br class="sp-only"><span class="navbar__item__text">投稿</span>
+      </a>
+    </li>
+    <li class="navbar__item">
+      <a href="#">
+        <span class="navbar__item__icon"><font-awesome-icon icon="bell"/></span><br class="sp-only"><span class="navbar__item__text">通知</span>
+      </a>
+    </li>
+    <li class="navbar__item">
+      <a href="#">
+        <span class="navbar__item__icon"><font-awesome-icon icon="cog"/></span><br class="sp-only"><span class="navbar__item__text">設定</span>
+      </a>
+    </li>
   </ul>
 </template>
 
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  components:{
+    FontAwesomeIcon
+  }
 }
 </script>
--- data/www/src/main.js
+++ data/www/src/main.js
@@ -6,6 +6,19 @@ import http from './http'
 import 'musubii/src/scss/musubii.scss'
 require('@/assets/scss/application.scss')
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faSearch, faEdit, faBell, faCog } from '@fortawesome/free-solid-svg-icons'
+// import { faComment } from '@fortawesome/free-regular-svg-icons'
+// import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faHome,
+  faSearch,
+  faEdit,
+  faBell,
+  faCog
+)
+
 const app = createApp(App)
 app.use(router)
 app.use(http);
